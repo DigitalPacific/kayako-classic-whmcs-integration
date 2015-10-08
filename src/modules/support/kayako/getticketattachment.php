ae8cb4727b65c21e598409f8fa57c31336fe48bb
mansiwason <mansi.wason@kayako.com>
2015-10-08 16:15:50 +0530

    Audit change [skip ci]

--- src/modules/support/kayako/getticketattachment.php
+++ src/modules/support/kayako/getticketattachment.php
@@ -5,12 +5,12 @@
  * SWIFT Framework
  * _______________________________________________
  *
- * @author		Ruchi Kothari
+ * @author         Ruchi Kothari
  *
- * @package		SWIFT
- * @copyright	Copyright (c) 2001-2015, Kayako
- * @license		http://www.kayako.com/license
- * @link		http://www.kayako.com
+ * @package        SWIFT
+ * @copyright      Copyright (c) 2001-2015, Kayako
+ * @license        http://www.kayako.com/license
+ * @link           http://www.kayako.com
  *
  * ###############################################
  */
